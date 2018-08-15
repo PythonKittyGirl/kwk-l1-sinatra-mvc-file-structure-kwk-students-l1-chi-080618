c045f50b7ff6a907e12040d997384975676ea2b9
PythonKittyGirl <ung.deborah@gmail.com>
2018-08-15 13:58:18 +0000

    automatically backed up by learn

--- config.ru
+++ config.ru
@@ -1,3 +1,14 @@
 require_relative './config/environment'
 
 run ApplicationController
+
+class Dog
+  
+def initialize Dog(name = "rudolph", breed = "mastiff", age = 2)
+  @name = name
+  @breed = mastiff
+  @age = age
+  puts "#{name} is a #{breed} and is #{age} years old."
+   @dog.name = "bessie"
+end
+  end
---
+++ hlp.rb
@@ -0,0 +1,31 @@
+require_relative './config/environment'
+
+run ApplicationController
+
+class Dog
+  
+def initialize Dog(name, breed, age)
+  @name = name
+  @breed = mastiff
+  @age = age
+  puts "#{name} is a #{breed} and is #{age} years old."
+ class Dog << Dog.new("rudolph", "mastiff", 2)
+   @dog.name = "bessie"
+end
+  end
+  
+  
+  require_relative './config/environment'
+
+run ApplicationController
+
+class Dog
+  
+def initialize Dog(name = "rudolph", breed = "mastiff", age = 2)
+  @name = name
+  @breed = mastiff
+  @age = age
+  puts "#{name} is a #{breed} and is #{age} years old."
+   @dog.name = "bessie"
+end
+  end
